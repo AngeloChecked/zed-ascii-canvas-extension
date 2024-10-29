d3302619dbb79408e601abe6d740ec693d5af796
AngeloChecked <angi99552@gmail.com>
2024-10-30 00:56:10 +0100

    fix highlights

--- languages/asciicanvas/highlights.scm
+++ languages/asciicanvas/highlights.scm
@@ -1,7 +1,6 @@
-white @operator
-black @keyword
-red @type
-blue @number
-yellow @function
-green @variable
-text @string
+(white) @operator
+(black) @keyword
+(red) @type
+(blue) @number
+(yellow) @function
+(green) @variable
